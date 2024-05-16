012c122b1a7ae5f7e769f831c3f6944d71760b6e
gangadharbsk <gangadharbsk@gmail.com>
2024-05-16 15:57:05 +0530

    trivy scaning image

--- vars/dockerimagescan.groovy
+++ vars/dockerimagescan.groovy
@@ -1,6 +1,5 @@
 def call(String project, String Imagetag, String hubUser){
     sh """
-        trivy image
         trivy image ${hubUser}/${project}:latest > scan.txt
         cat scan.txt
         """
