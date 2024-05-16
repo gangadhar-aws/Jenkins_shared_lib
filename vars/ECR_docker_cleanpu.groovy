debf63e5da1c1d766a8b56e691f99841e5595e54
kumari@gmail.com <kumari>
2024-05-16 20:54:41 +0530

    cleanpu stage updated

--- vars/ECR_docker_cleanpu.groovy
+++ vars/ECR_docker_cleanpu.groovy
@@ -1,5 +1,5 @@
 def call(String aws_account_id, String region, String ecr_reponame){
     sh """
-       docker rmi ${ecr_reponame}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest 
+       docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest 
     """
 }
