372d55b375dc807589a4cc2df265def7fe14210c
gangadharbsk <gangadharbsk@gmail.com>
2024-05-16 19:19:44 +0530

    scan image updated

--- vars/ECR_docker_imagescan.groovy
+++ vars/ECR_docker_imagescan.groovy
@@ -1,6 +1,5 @@
 def call(String aws_account_id, String region, String ecr_reponame){
     sh """
-        trivy image ${aws_account_id}.
         trivy image ${ecr_reponame}.dkr.ecr.${region}.amazonaws.com/${ecr_reponame}:latest > scan.txt
         cat scan.txt
         """
